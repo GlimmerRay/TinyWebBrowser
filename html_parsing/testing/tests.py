fdedf7fdf393121e57dd6699a45bb202939d0d17
glimmerray <bhodges7@student.gsu.edu>
2021-03-09 13:25:39 -0500

    first basic parsing test passes

--- html_parsing/testing/tests.py
+++ html_parsing/testing/tests.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.insert(0, '/Users/brandonhodges/Desktop/tiny_web_browser/')
 import os
-from html_parsing.html_parser import parse
+from html_parsing.html_parser import *
 
 TEST_DIR = 'html_test_files/'
 
@@ -15,6 +15,20 @@ def load_file(filename):
     return contents
 
 if __name__ == '__main__':
+    assert consume_elem_name('<div>', 0) == ('div', 4)
+    assert consume_elem_name('abcdefg<div>', 7) == ('div', 11)
+    assert consume_elem_name('<fruit>', 0) == ('fruit', 6)
+    assert consume_elem_name('abcdefg<fruit>', 7) == ('fruit', 13)
+    
     test1 = load_file('test1.html')
-    parse(test1)
+    dom = parse(test1)
+    assert dom.name == 'html'
+    assert len(dom.children) == 2
+    assert dom.children[0].name == 'head'
+    assert dom.children[1].name == 'body'
+    assert len(dom.children[0].children) == 1
+    assert dom.children[0].children[0].name == 'title'
+    assert len(dom.children[0].children[0].children) == 1
+    assert dom.children[0].children[0].children[0].text == 'Document'
+    assert len(dom.children[1].children) == 0
 
